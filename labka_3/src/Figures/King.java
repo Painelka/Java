239c2d9ad189e8c7a90af813e09230fd6c0cb583
Софья <113128986+Painelka@users.noreply.github.com>
2024-11-14 21:34:30 +0200

    Update King.java

--- labka_3/src/Figures/King.java
+++ labka_3/src/Figures/King.java
@@ -6,4 +6,24 @@ public class King extends Figure {
     public King(String name, char color) {
         super(name, color);
     }
+      @Override
+    public boolean canMove(int row, int col, int row1, int col1) {
+        if (Math.abs(row - row1) <= 1 && Math.abs(col - col1) <= 1) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean canAttack(int row, int col, int row1, int col1) {
+        return canMove(row, col, row1, col1);
+    }
+
+    public boolean isFirstMove() {
+        return isFirstMove;
+    }
+
+    public void setFirstMove(boolean firstMove) {
+        isFirstMove = firstMove;
+    }
 }
