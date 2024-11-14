d225a4b4c0717c03cceed4094fb415ef3f8e41ed
Софья <113128986+Painelka@users.noreply.github.com>
2024-11-14 21:57:02 +0200

    Update Rook.java

--- labka_3/src/Figures/Rook.java
+++ labka_3/src/Figures/Rook.java
@@ -6,4 +6,21 @@ public class Rook extends Figure {
     public Rook(String name, char color) {
         super(name, color);
     }
+        @Override
+    public boolean canMove(int row, int col, int row1, int col1) {
+        return (row == row1 || col == col1);
+    }
+
+    @Override
+    public boolean canAttack(int row, int col, int row1, int col1) {
+        return canMove(row, col, row1, col1);
+    }
+
+    public boolean isFirstMove() {
+        return isFirstMove;
+    }
+
+    public void setFirstMove(boolean firstMove) {
+        isFirstMove = firstMove;
+    }
 }
